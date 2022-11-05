d537d8d9c744500e997ec3e9172c13d5bbf4ec92
byrod <77072122+byrod-de@users.noreply.github.com>
2022-11-05 22:47:59 +0100

    added ranked war command

---
+++ commands/rankedwars.js
@@ -0,0 +1,101 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { apiKey, comment } = require('../config.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('rankedwars')
+		.setDescription('Get a list of Torn Ranked Wars'),
+
+	async execute(interaction) {
+    console.log(`Used API Key ${apiKey} `);
+
+    let rwURL = 'https://api.torn.com/torn/?selections=rankedwars&key=' + apiKey + '&comment=' + comment;
+    console.log(` > ${rwURL}`);
+
+    let rwResponse = await fetch(rwURL);
+
+    let fieldsAdded = 0;
+
+    if (rwResponse.ok) { // if HTTP-status is 200-299
+      // get the response body (the method explained below)
+      let jsonRwResponse = await rwResponse.json();
+      let rankedWars = jsonRwResponse['rankedwars'];
+
+      let rwEmbed = new EmbedBuilder()
+      .setColor(0x1199bb)
+      .setTitle('Ranked Wars')
+      .setDescription('Ranked Wars which ended within the last 5 hours')
+      .setTimestamp();
+
+      for( let rwId in rankedWars ){
+
+    		let rankedWar = rankedWars[rwId];
+
+    		let faction1Name = '', faction2Name  = '', faction1ID = '', faction2ID  = '', faction1Score = '', faction2Score  = '';
+    		let counter = 0;
+
+    		let warStatus = '', rankedWarReport;
+        let faction1Items = '', faction2Items = '';
+        let now = Date.now() / 1000;
+
+
+        if ((rankedWar.war.end != 0) && (now -  rankedWar.war.end <= 18000) && (fieldsAdded <= 25))) {
+
+          let rwReportlURL = 'https://api.torn.com/torn/' + rwId + '?selections=rankedwarreport&key=' + apiKey + '&comment=' + comment;
+          console.log(` >> ${rwReportlURL}`);
+
+          let rwReportResponse = await fetch(rwReportlURL);
+
+          if (rwReportResponse.ok) {
+            let jsonRwReportResponse = await rwReportResponse.json();
+            rankedWarReport = jsonRwReportResponse['rankedwarreport'];
+          }
+
+          warStatus = 'ended';
+
+          console.log(` >>> ${warStatus}`);
+
+
+          for( let factionID in rankedWar.factions ){
+
+            let faction = rankedWar.factions[factionID];
+
+            if (counter == 0) {
+              faction1Name = faction.name;
+              faction1Score = faction.score;
+              faction1ID = factionID;
+              counter = 1;
+
+              for (let itemsId in rankedWarReport.factions[factionID].rewards.items) {
+                  faction1Items = faction1Items + rankedWarReport.factions[factionID].rewards.items[itemsId].quantity + 'x ' + rankedWarReport.factions[factionID].rewards.items[itemsId].name + '\n';
+              }
+
+            } else {
+              faction2Name = faction.name;
+              faction2Score = faction.score;
+              faction2ID = factionID;
+
+              for (let itemsId in rankedWarReport.factions[factionID].rewards.items) {
+                  faction2Items = faction2Items + rankedWarReport.factions[factionID].rewards.items[itemsId].quantity + 'x ' + rankedWarReport.factions[factionID].rewards.items[itemsId].name + '\n';
+              }
+            }
+          }
+
+          rwEmbed.addFields({ name: `${faction1Name}`, value: `Score: ${faction1Score}\n${faction1Items}\n`, inline: true });
+          rwEmbed.addFields({ name: `${faction2Name}`, value: `Score: ${faction2Score}\n${faction2Items}\n`, inline: true });
+          rwEmbed.addFields({ name: warStatus, value: `<t:${rankedWar.war.end}:R>`, inline: true });
+          rwEmbed.addFields({ name: '\u200B', value: '\u200B' });
+
+          fieldsAdded = fieldsAdded + 4;
+
+    		}
+      }
+
+      await interaction.reply({ embeds: [rwEmbed], ephemeral: true });
+
+
+    } else {
+      alert("HTTP-Error: " + response.status);
+    }
+	},
+};

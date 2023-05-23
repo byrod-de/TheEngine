e048f802bfc0bf5e7ee1d23554725fb6ec54a3ca
byrod <77072122+byrod-de@users.noreply.github.com>
2023-05-23 23:00:40 +0200

    updates status message

--- theEngine.js
+++ theEngine.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token, statusChannelId, statusGuildId } = require('./config.json');
+const { token, statusChannelId, statusGuildId, apiKey, comment } = require('./config.json');
 const moment = require('moment');
 const os = require('os');
 const hostname = os.hostname();
@@ -60,6 +60,24 @@ async function send_msg() {
 	let currentDate = moment().format().replace('T',' ');
 	let statusMessage = `Still running on ${hostname}! ${currentDate}`;
 	let statusChannel = client.channels.cache.get(statusChannelId);
+
+    let apiURL = `https://api.torn.com/torn/?selections=timestamp&key=${apiKey}&comment=${comment}`;
+    //console.log(` > ${apiURL}`);
+
+    let apiResponse = await fetch(apiURL);
+
+    if (apiResponse.ok) { // if HTTP-status is 200-299
+
+        let apiJson = await apiResponse.json();
+
+        if (apiJson.hasOwnProperty('error')) {
+            statusMessage = statusMessage + `\nError Code ${apiJson['error'].code},  ${apiJson['error'].error}.`;
+        } else {
+            statusMessage = statusMessage + `\nTorn API available!`;
+        }
+    } else {
+        statusMessage = statusMessage + `\nGeneral http error.`;
+    }
 
 	statusChannel.send(`\`\`\`${statusMessage}\`\`\``);
 }

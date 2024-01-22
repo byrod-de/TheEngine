cd2925c9153b8325f279ecf802bf9e0bb23205d6
byrod <77072122+byrod-de@users.noreply.github.com>
2024-01-22 19:58:59 +0100

    added diceroll and coinflip

--- commands/rollies.js
+++ commands/rollies.js
@@ -1,56 +1,57 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { printLog } = require('../helper/misc');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('rollies')
-        .setDescription('Challenge someone to a rollies game!')
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user you want to challenge.')
-                .setRequired(true)),
+        .setDescription('Roll the dice!!')
+        .addIntegerOption(option =>
+            option.setName('dice')
+            .setDescription('Select your dice')
+            .addChoices(
+                { name: 'Roll a D20', value: 20 },
+                { name: 'Roll a D12', value: 12 },
+                { name: 'Roll a D10', value: 10 },
+                { name: 'Roll a D8', value: 8 },
+                { name: 'Roll a D6', value: 6 },
+                { name: 'Roll a D4', value: 4 },
+                { name: 'Coinflip', value: 2 },
+            )),
 
     async execute(interaction) {
-        const challenger = interaction.user;
-        const challenged = interaction.options.getUser('user');
+        const dice = interaction.options.getInteger('dice') ?? 20;
 
-        if (!challenged) {
-            await interaction.reply('You need to challenge someone. Mention a user.');
-            return;
-        }
-
-        const roll = () => Math.floor(Math.random() * 20) + 1;
-        const challengerRoll = roll();
-        let challengedRoll = null;
+        let result = Math.floor(Math.random() * dice) + 1;
 
         const rolliesEmbed = new EmbedBuilder()
-            .setColor(0xdf691a)
-            .setTitle('Rollies Game')
-            .setDescription(`${challenger.username} challenged ${challenged.username} to a rollies game!`)
-            .addFields({ name: 'Rolls', value: `${challenger.username}: ${challengerRoll}\n${challenged.username}: ${challengedRoll || 'Waiting for roll...'}`, inline: false },
-                { name: 'Result', value: 'It\'s a tie!', inline: false })
-            .setTimestamp()
-            .setFooter({ text: 'powered by TornEngine', iconURL: 'https://tornengine.netlify.app/images/logo-100x100.png' });
+        .setColor(0xdf691a)
+        .setTitle(`Rollies!`)
+        .setDescription(`Roll the dice!`)
+        .setTimestamp()
+        .setFooter({ text: 'powered by TornEngine, inspired be Critical Role', iconURL: 'https://tornengine.netlify.app/images/logo-100x100.png' });
 
-        // Send the initial message
-        const message = await interaction.reply({ embeds: [rolliesEmbed], ephemeral: true });
-
-        // Wait for challenged player to confirm with a reaction
-        await message.react('✅');
-
-        const filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === challenged.id;
-        const collector = message.createReactionCollector({ filter, time: 30000 });
-
-        collector.on('collect', async () => {
-            challengedRoll = roll();
-            rolliesEmbed.fields[0].value = `${challenger.username}: ${challengerRoll}\n${challenged.username}: ${challengedRoll}`;
-            rolliesEmbed.fields[1].value = `${challenged.username} rolled!`;
-            await interaction.editReply({ embeds: [rolliesEmbed] });
-        });
-
-        collector.on('end', collected => {
-            if (collected.size === 0) {
-                interaction.followUp('Rollies game ended because the challenged player did not confirm the roll in time.');
+        if (dice == 2) {
+            if (result == 1) {
+                rolliesEmbed.addFields(
+                    { name: 'Heads', value: 'Result Coinflip', inline: false },
+                );
+            } else {
+                rolliesEmbed.addFields(
+                    { name: 'Tails', value: 'Result Coinflip', inline: false },
+                );
             }
-        });
-    },
+        } else {
+            if (result == 1) {
+                result = 'Natural 1 *sad trombone*';
+            }
+
+            if (result == 20) {
+                result = 'Natural fucking 20!';
+            }
+            rolliesEmbed.addFields(
+                { name: `${result}`, value: `Result dice roll D${dice}`, inline: false },
+            );
+        }
+        await interaction.reply({ embeds: [rolliesEmbed], ephemeral: true });
+    }
 };
